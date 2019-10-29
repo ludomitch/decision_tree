8c6e1e03ef39d8b80692d0804fcea1c96772ae2d
sachahu1 <sachahu@hotmail.fr>
2019-10-29 13:30:32 +0100

    cross validation structure finished, missing pruning and choosing tree

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,5 @@
 import numpy as np
-from tree import predict
-
+from tree import predict, tree_learn
 
 def split(arr, pos, n):
     """method : split : takes an array, splits it in 2 uneven arrays.
@@ -16,63 +15,61 @@ def split(arr, pos, n):
     return upper, lower
 
 
-def cross_validation(tree, data, folds, test_percentage):
+def cross_validation(data, folds, test_percentage):
     """method : cross_validation : we'll explain once it's finished
-        tree : dictionary of dictionaries: this is the decision tree
         data : array : this is the complete dataset used
         folds : int : how many folds are used during cross validation
         test_percentage : float : the percentage of the dataset to be used for testing
         returns "we'll see"""
+    
+    # copy data to avoid damaging the dataset
     df = data.copy()
-    errorList = []
     split_size = int(test_percentage * df.shape[0])
+    
+    tree_structures = {} # Trees trained on different test sets
+    tree_dictionary = {} # Trees trained on the same Validation + Training
 
-    luap, luar, lf1, luac = [], [], [], []
-
+    # Keep index before shuffling
     # index = np.arange(df.shape[0])
     # np.hstack((df, index)) # adding an index before shuffling
     np.random.shuffle(df)
 
     # Splitting Test from Validation and Training
     for i in range(folds):
+        print("-------------------- Test/Train separation "+ str(i)+" --------------------")
         test_set, train_and_validate = split(df, i * split_size, split_size)
-        # print("Test, Val")
-        # print("split Position:"+str(i*split_size))
-        uar = 0
-        uap = 0
-        uac = 0
-        f1 = 0
-
+        
         # Splitting Validation from Training
+        variance = np.zeros((4,folds-1))
         for j in range(folds - 1):
+            print("------- Train/Validate separation "+ str(j)+" -------")
+            # Split Validation from Training
             validate, train = split(train_and_validate, j * split_size, split_size)
-            tuar, tuap, tf1, tuac = evaluate(tree, validate, 4)  # for later use
-            uar += tuar
-            uap += tuap
-            f1 += tf1
-            uac += tuac
+            # Train Tree on Training
+            print("Training Tree")
+            tree, depth = tree_learn(train, depth = 3, tree = {})
+            print("Pruning not done yet\n")
+            # Pruning
+            
+            # Evaluate Tree on Validate
+            uar, uap, f1, uac = evaluate(tree, validate, 4)  # for later use
+            variance[0,j] = uar
+            variance[1,j] = uap
+            variance[2,j] = f1
+            variance[3,j] = uac
+            tree_dictionary["tree_" + str(j)] = {"tree" : tree, "UAR" : uar, "UAP" : uap, "F1" : f1, "UAC" : uac}
 
-            # Train tree on train
-            # validate
-            # print("Val, train")
-            # print("split Position:"+str(j*split_size))
-            # compute errors
-        # errorList.append()
-        luar.append(uar / j)
-        luap.append(uap / j)
-        lf1.append(f1 / j)
-        luac.append(uac / j)
-    # luar = np.mean(luar)
-    # luap = np.mean(luap)
-    # luac = np.mean(luac)
-    # lf1 = np.mean(lf1)
+        print("\n----------- Compute Variances -----------\n")
+        # Compute Variance on UAR, UAP, F1 and UAC (in this order)
+        print(variance.shape)
+        variance = np.var(variance, axis = 1)
+        print(variance.shape)
+        print("UAR : %.5f   UAP : %.5f   F1 : %.5f   UAC : %.5f"%(variance[0], variance[1], variance[2], variance[3]))
+        print("\n\n")
+        # Each structure stores n-1 trees and the variances computed
+        tree_structures["structure_"+str(i)] = {"tree_struct" : tree_dictionary, "variances" : variance}
 
-    print("uar = ", luar, end="\n\n")
-    print("uap = ", luap, end="\n\n")
-    print("Classification Rate = ", luac, end="\n\n")
-    print("f1 = ", lf1, end="\n\n")
-
-    return np.mean(lf1) # return f1 fr the moment.
+    return tree_structures
 
 
 def evaluate(tree, validation_set, nb_labels):
@@ -82,45 +79,40 @@ def evaluate(tree, validation_set, nb_labels):
     nb_labels : int : number of labels to classify
     returns uar, uap, f1 and the classification rate (see lecture notes)"""
 
+    # Create the confusion matrix
     confusion_matrix = np.zeros((nb_labels, nb_labels))
-    for i in range(validation_set.shape[0]):
-        if predict(tree, validation_set[i, :]) == validation_set[i, -1]:
+    
+    for i in range(validation_set.shape[0]): # For each line from the validation set
+        
+        if predict(tree, validation_set[i, :]) == validation_set[i, -1]: # If the tree predicted the label correctly
             confusion_matrix[
                 int(validation_set[i, -1]) - 1, int(validation_set[i, -1]) - 1
-            ] += 1
+            ] += 1 # At position (correct label, correct label) we add 1
 
-        elif predict(tree, validation_set[i, :]) != validation_set[i, -1]:
+        elif predict(tree, validation_set[i, :]) != validation_set[i, -1]: # If the tree predicted wrongly
             confusion_matrix[
                 int(validation_set[i, -1]) - 1,
                 int(predict(tree, validation_set[i, :])) - 1,
-            ] += 1
+            ] += 1 # At position (correct label, predicted label) we add 1
 
+    # Initializing Recall, Precision and Classification rate
     recall_vect = np.zeros((1, nb_labels))
     prec_vect = np.zeros((1, nb_labels))
     classification_rate = np.zeros((1, nb_labels))
 
     for i in range(confusion_matrix.shape[0]):
-        recall_vect[0, i] = confusion_matrix[i, i] / np.sum(confusion_matrix[i, :])
-        prec_vect[0, i] = confusion_matrix[i, i] / np.sum(confusion_matrix[:, i])
+        recall_vect[0, i] = confusion_matrix[i, i] / np.sum(confusion_matrix[i, :]) # Recall = TP/(TP + FN)
+        prec_vect[0, i] = confusion_matrix[i, i] / np.sum(confusion_matrix[:, i]) # Precision = TP/(TP + FP)
         classification_rate[0, i] = np.trace(confusion_matrix) / (
             np.trace(confusion_matrix)
             + np.sum(confusion_matrix[:, i])
             + np.sum(confusion_matrix[i, :])
             - 2 * confusion_matrix[i, i]
-        )
+        ) # Classification rate = (TP + TN)/(TP + TN + FP + FN)
 
-    uar = np.mean(recall_vect)
-    uap = np.mean(prec_vect)
-    f1 = 2 * (uar * uap) / (uar + uap)
-    uaclassifcation_rate = np.mean(classification_rate)
-
-    # print("Recall [Room 1 | Room 2 | Room 3 | Room 4]")
-    # print(recall_vect, end= "uar ="+str(uar) +"\n\n")
-    # print("Precision [Room 1 | Room 2 | Room 3 | Room 4]")
-    # print(prec_vect, end= "uap ="+str(uap) +"\n\n")
-    # print("Classification Rate [Room 1 | Room 2 | Room 3 | Room 4]")
-    # print(classification_rate, end= "Average ="+str(uar) +"\n\n")
-
-    # print(confusion_matrix)
+    uar = np.mean(recall_vect) # Unweighted Average Recall
+    uap = np.mean(prec_vect) # Unweighted Average Precision
+    f1 = 2 * (uar * uap) / (uar + uap) # Compute F1
+    uaclassifcation_rate = np.mean(classification_rate) # Unweighted Averate Classification Rate
 
     return uar, uap, f1, uaclassifcation_rate
