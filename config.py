3f68a9c31db736cad5d929ac4ed75726b306a734
sachahu1 <sachahu@hotmail.fr>
2019-11-03 17:08:59 +0100

    oups

---
+++ config.py
@@ -0,0 +1,9 @@
+NB_LABELS = 4
+METRIC_CHOICE = "f1" # f1, uar, uac, uap
+
+# HYPERPARAMETERS
+DEPTHS = [2, 4, 6, 8, 10, 12]
+BOUNDARIES = [2]
+PRUNING = [True]
+
+DATASET = "clean"  # clean or noisy
